73e45f1eb06ef2c3559f65c3634320dcb23e0697
pork3 <zbower1@gmail.com>
2017-10-01 15:33:43 -0700

    cleaned up root directory

--- spew/argparse_test.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(
-    description="Sample to test 'dest' for positional arg")
-parser.add_argument("foo", dest="FOO", 
-                        help="is this ok?")
-
-    
